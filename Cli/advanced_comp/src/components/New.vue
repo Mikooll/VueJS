a697de4e106b52147af8d4eb33a1b0e526ce52dc
M. Mare <michael.mare@soprasteria.com>
2020-07-01 16:45:35 +0200

    added dynamic content

---
+++ Cli/advanced_comp/src/components/New.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Increase</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0,
+      };
+    },
+  };
+</script>
+
+<style></style>
